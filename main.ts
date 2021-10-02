8223f2648eeac7156372f638df9e49722092c22e
nsirirak <33281043+nsirirak@users.noreply.github.com>
2021-10-02 04:33:09 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ namespace Timer {
      * Call a function periodicly 
      */
     //% blockId=periodicBlock
-    //% block="Interval $p $f"
+    //% block="Interval $p"
     //% p.min=1 p.max=1000
     export function periodic( p: number, f: () =>void) {
         control.setInterval(f,p, control.IntervalMode.Interval)
